d79bb353c06cb325a82e4bfb092fca9b6048eba4
TotsukaYutaro <totsukayutaro@totbook.local>
2016-11-21 00:19:36 +0900

    added samples

---
+++ src/js/sample.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Button } from 'react-bootstrap';
+
+render(
+  <Button>Press here!</Button>,
+  document.getElementById('react-component')
+);
+
+
